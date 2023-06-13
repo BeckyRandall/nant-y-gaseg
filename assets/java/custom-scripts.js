13cc50107415c7d31869ff56ed0df417eb723042
GeraintRandall <Geraint@Geraints-MacBook.local>
2023-06-13 22:14:45 +0100

    Draft 2

--- assets/java/custom-scripts.js
+++ assets/java/custom-scripts.js
@@ -15,45 +15,37 @@ function fastfade() {
 
 //stay
 document.getElementById("info-nav-stay").addEventListener("mouseover", function () {
-    console.log("on")
-    document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/barnview.webp)";
+    document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/barnview-info.webp)";
 }, false);
 
 document.getElementById("info-nav-stay").addEventListener("mouseout", function () {
-    console.log("off")
     document.getElementById("info-nav-center").style.backgroundImage = "";
 }, false);
 
 //celebrate
 document.getElementById("info-nav-celebrate").addEventListener("mouseover", function () {
-    console.log("on")
-    document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/carousel1.webp)";
+    document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/weddingwellies-info.webp)";
 }, false);
 
 document.getElementById("info-nav-celebrate").addEventListener("mouseout", function () {
-    console.log("off")
     document.getElementById("info-nav-center").style.backgroundImage = "";
 }, false);
 
 //retreat
 document.getElementById("info-nav-retreat").addEventListener("mouseover", function () {
-    console.log("on")
-    document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/DSC_9129-Edit.webp)";
+    document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/retreat-hero-info.webp)";
 }, false);
 
 document.getElementById("info-nav-retreat").addEventListener("mouseout", function () {
-    console.log("off")
-    document.getElementById("info-nav-retreat").style.backgroundImage = "";
+    document.getElementById("info-nav-center").style.backgroundImage = "";
 }, false);
 
 //blog
 document.getElementById("info-nav-blog").addEventListener("mouseover", function () {
-    console.log("on")
     document.getElementById("info-nav-center").style.backgroundImage = "url(../assets/imgs/wildflowerfield.webp)";
 }, false);
 
 document.getElementById("info-nav-blog").addEventListener("mouseout", function () {
-    console.log("off")
     document.getElementById("info-nav-center").style.backgroundImage = "";
 }, false);
 
