e6a41eca25b4b4908f5487d389bd4886166ef8b2
BeckyRandall <rebecca-beatrice@hotmail.co.uk>
2023-08-28 09:07:35 +0100

    Carousels and Logo

--- assets/java/custom-scripts.js
+++ assets/java/custom-scripts.js
@@ -12,6 +12,22 @@ function fastfade() {
 
 
 //INFO NAV CENTRE CIRCLE IMAGE REPLACEMENT
+//--
+//var images = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+
+//-- usage --//
+preload(
+    "../assets/imgs/barnview-info.webp",
+    "../assets/imgs/weddingwellies-info.webp",
+    "../assets/imgs/retreat-hero-info.webp",
+    "../assets/imgs/wildflowerfield.webp"
+);
 
 //stay
 document.getElementById("info-nav-stay").addEventListener("mouseover", function () {
